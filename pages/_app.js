9006b32ea996227758c0e66d613e46e43b9af23d
Baiji He <baiji@Mac.attlocal.net>
2024-12-04 21:10:27 -0800

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '@styles/globals.css'
+import Script from 'next/script'
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<>
+    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-VKCLQ5C1LG"></Script>
+    <Script id="google-analytics">
+      {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-VKCLQ5C1LG');
+      `}
+    </Script>
+    <Component {...pageProps} />
+  </>);
 }
 
 export default Application
